469d6ef8c261089962f203a05ebf9c53278c9323
Guanchu Wang <gary@Mac.attlocal.net>
2025-11-03 20:21:32 -0500

    add use case

--- inference_example.py
+++ inference_example.py
@@ -1,19 +1,18 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 from decoding_tree_sketching.kvbatch_decoder import KVBatchEGDT
 
 examples = [
-    "Find the sum of all integer bases $b>9$ for which $17_b$ is a divisor of $97_b.$",
+    "Let $\\mathcal{B}$ be the set of rectangular boxes with surface area $54$ and volume $23$. Let $r$ be the radius of the smallest sphere that can contain each of the rectangular boxes that are elements of $\\mathcal{B}$. The value of $r^2$ can be written as $\\\frac{p}{q}$, where $p$ and $q$ are relatively prime positive integers. Find $p+q$.",
+]
+groundtruths = [
+    "721",
 ]
 reasoning_tail = r" Please reason step by step, and put your final answer within \boxed{}."
+seed = 0
 
 # Replace with your model checkpoint if needed
 model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-# TODO: Replace with your Hugging Face access token
-access_token = "YOUR_HF_ACCESS_TOKEN"
-# TODO: Set a directory for model/tokenizer cache
-cache_dir = "./YOUR_CACHE_DIR"
-seed = 0
 
 # Decoding hyperparameters
 DECODE_CONFIG = {
@@ -25,42 +24,56 @@ DECODE_CONFIG = {
 }
 
 def main():
+    set_seed(seed)
     tokenizer = AutoTokenizer.from_pretrained(
         model_name,
-        token=access_token,
-        cache_dir=cache_dir,
         trust_remote_code=True
     )
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         device_map="cuda",
         torch_dtype="auto",
-        token=access_token,
-        cache_dir=cache_dir,
         trust_remote_code=True
     )
+    streamer = TextStreamer(tokenizer)
 
     kvegdt = KVBatchEGDT(model, tokenizer, seed=seed)
 
-    for example in examples:
-        full_prompt = example + reasoning_tail
-        text = tokenizer.apply_chat_template(
-            [{"role": "user", "content": full_prompt}],
-            tokenize=False,
-            add_generation_prompt=True
-        )
-        out = kvegdt.generate(
-            text,
-            entropy_threshold=DECODE_CONFIG["entropy_threshold"],
-            branch_top_k=DECODE_CONFIG["branch_top_k"],
-            max_active_hyps=DECODE_CONFIG["max_active_hyps"],
-            max_new_tokens=DECODE_CONFIG["max_new_tokens"],
-            temperature=DECODE_CONFIG["temperature"],
-        )
-        
-        print(f"*** MODEL OUTPUT ***\n{out['text']}")
-        # Print generation statistics such as steps, branch events, and sequence length
-        print(f"\n*** GENERATION STATS ***\n{out['stats']}")
+    ques_idx = 0
+    example = examples[ques_idx]
+    groundtruth = groundtruths[ques_idx]
+    full_prompt = example + reasoning_tail
+    text = tokenizer.apply_chat_template(
+        [{"role": "user", "content": full_prompt}],
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    # Regular output
+    inputs = tokenizer(text, return_tensors="pt").to(model.device)
+    out = model.generate(
+        **inputs,
+        max_new_tokens=DECODE_CONFIG["max_new_tokens"],
+        do_sample=True,     
+        temperature=DECODE_CONFIG["temperature"],    
+        streamer=streamer,
+    )
+
+    # DTS output
+    dts_out = kvegdt.generate(
+        text,
+        entropy_threshold=DECODE_CONFIG["entropy_threshold"],
+        branch_top_k=DECODE_CONFIG["branch_top_k"],
+        max_active_hyps=DECODE_CONFIG["max_active_hyps"],
+        max_new_tokens=DECODE_CONFIG["max_new_tokens"],
+        temperature=DECODE_CONFIG["temperature"],
+    )
+    
+    print(f"*** MODEL OUTPUT ***\n{dts_out['text']}")
+    # Print generation statistics such as steps, branch events, and sequence length
+    print(f"\n*** GENERATION STATS ***\n{dts_out['stats']}")
+
+    print(f"Groundtruth = {groundtruth}, Regular decoding output = {}, DTS output = {dts_out['stats']}")
 
 if __name__ == "__main__":
     main()       
--- notebooks/README.md
+++ notebooks/README.md
@@ -1,3 +1,1 @@
-## 🚀 Run DTS on DeepSeek-R1-Distill-Qwen-1.5B 
-
-[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ZichengXu/Decoding-Tree-Sketching/blob/main/notebooks/example-DeepSeek_R1_Distill_Qwen_1_5B.ipynb)
+## 🚀 Run DTS on DeepSeek-R1-Distill-Qwen-1.5B with Colab [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/ZichengXu/Decoding-Tree-Sketching/blob/main/notebooks/example-DeepSeek_R1_Distill_Qwen_1_5B.ipynb)
